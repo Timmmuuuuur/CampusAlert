e9499c35f3948e554765aacaaa3379a46ccadbaa
Kevin Lee <k323lee@uwaterloo.ca>
2023-07-13 19:54:36 -0400

    Combine two branches

--- playground/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-#URLconfiguration
-urlpatterns = [
-    # path('', views.HomeView.as_view()),
-    path('', views.index, name='index'),
-    path('test_graphs', views.ChartData.as_view())
-]
