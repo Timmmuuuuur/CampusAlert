cf424f719416dde8b9ed253f75bf0b0661fdde9f
Kevin Lee <kevinleefm@hotmail.com>
2023-11-06 00:51:50 -0500

    Created API endpoints for all the models

--- backend/emergency/urls.py
+++ backend/emergency/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import notification_test, create_floor, create_floorlayout, get_floorlayout_image_url, create_rooms, create_building
+from .views import BuildingView, FloorLayoutView, FloorView, RoomEdgeView, notification_test, create_floor, create_floorlayout, get_floorlayout_image_url, create_rooms, create_building, RoomNodeView, history_latest_change
 
 urlpatterns = [
     path('notificationtest', notification_test, name='notification_test'),
@@ -12,5 +12,13 @@ urlpatterns = [
     path('building/create', create_building, name='create_rooms'),
 
     # GET endpoints
-    path('floorlayout/get_image_url', get_floorlayout_image_url, name='get_floorlayout_image_url')
+    path('floorlayout/get_image_url', get_floorlayout_image_url, name='get_floorlayout_image_url'),
+    path('room/node/all', RoomNodeView.as_view(), name='all_roomnodes'),
+    path('room/edge/all', RoomEdgeView.as_view(), name='all_roomedges'),
+    path('building/all', BuildingView.as_view(), name='all_buildings'),
+    path('floor/all', FloorView.as_view(), name='all_floors'),
+    path('floorlayout/all', FloorLayoutView.as_view(), name='all_floorlayouts'),
+
+    path('changes/latest', history_latest_change, name='history_latest_change')
+
 ]
--- backend/emergency/views.py
+++ backend/emergency/views.py
@@ -1,13 +1,18 @@
+import numpy as np
 import pandas as pd
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 from fcm_django.models import FCMDevice
 from firebase_admin.messaging import Message, Notification
+from rest_framework import generics
+from rest_framework.parsers import JSONParser
+from .serializers import RoomNodeSerializer, RoomEdgeSerializer, FloorLayoutSerializer, FloorSerializer, BuildingSerializer
 
 from .forms import BuildingForm, FloorForm, FloorLayoutForm, UploadRoomCSVForm
-from .models import Coordinate, Floor, FloorLayout, RoomEdge, RoomNode
+from .models import Coordinate, Floor, FloorLayout, RoomEdge, RoomNode, Building
 
 
 def call_notification(title, body):
@@ -55,13 +60,13 @@ def create_floor(request):
             name = form.cleaned_data['name']
 
             Floor.objects.create(
-                building = building,
-                floor_number = floor_number,
-                name = name,
-                layout = layout,
-                topleft = topleft,
-                topright = topright,
-                bottomleft = bottomleft,
+                building=building,
+                floor_number=floor_number,
+                name=name,
+                layout=layout,
+                topleft=topleft,
+                topright=topright,
+                bottomleft=bottomleft,
             )
 
             # Add a success message to the context
@@ -119,7 +124,6 @@ def create_building(request):
 def get_floorlayout_image_url(request):
     name = request.GET.get('name', '')
     floor_layout = get_object_or_404(FloorLayout, name=name)
-    print(floor_layout)
 
     # Check if layout_image is not empty
     if floor_layout.layout_image:
@@ -131,6 +135,20 @@ def get_floorlayout_image_url(request):
         return JsonResponse({'error': 'Image not found for this name.'})
     
 
+def get_last_edit(request):
+    name = request.GET.get('name', '')
+    floor_layout = get_object_or_404(FloorLayout, name=name)
+
+    # Check if layout_image is not empty
+    if floor_layout.layout_image:
+        # Return the URL to the image
+        image_url = floor_layout.layout_image.url
+        return JsonResponse({'image_url': image_url})
+    else:
+        # Handle cases where layout_image is empty
+        return JsonResponse({'error': 'Image not found for this name.'})
+
+
 def parse_csv(form, name):
     try:
         # Attempt to read and parse CSV files
@@ -141,19 +159,22 @@ def parse_csv(form, name):
 
 
 NODES_COLUMNS = ['name', 'x', 'y', 'is_exit']
-EDGES_COLUMNS = ['start_floor', 'start_room_name', 'end_floor', 'end_room_name']
+EDGES_COLUMNS = ['start_floor', 'start_room_name',
+                 'end_floor', 'end_room_name']
+
 
 def check_columns(form, df, columns, name):
     if all(col in df.columns for col in columns):
         return
     else:
         missing_columns = [col for col in columns if col not in df.columns]
-        form.add_error(name, f'Desired columns {missing_columns} are missing from the .csv.')
-        raise Exception(f"Desired columns {missing_columns} are missing from the .csv.")
+        form.add_error(
+            name, f'Desired columns {missing_columns} are missing from the .csv.')
+        raise Exception(
+            f"Desired columns {missing_columns} are missing from the .csv.")
 
 
 def create_rooms(request):
-    
     if request.method == 'POST':
         form = UploadRoomCSVForm(request.POST, request.FILES)
         if form.is_valid():
@@ -171,26 +192,26 @@ def create_rooms(request):
                 # print(edges_csv)
 
                 ignored_rooms = set()
-                
+
                 for _, row in nodes_csv.iterrows():
                     rd = row.to_dict()
-                    
+
                     # print(rd, form.cleaned_data['floor'])
 
                     if RoomNode.objects.filter(floor=floor, name=rd['name']).first() is None:
                         RoomNode.objects.create(
-                            floor = floor,
-                            name = rd['name'],
-                            x = rd['x'],
-                            y = rd['y'],
-                            is_exit = rd['is_exit']
+                            floor=floor,
+                            name=rd['name'],
+                            x=rd['x'],
+                            y=rd['y'],
+                            is_exit=rd['is_exit']
                         )
                     else:
                         ignored_rooms.add(rd['name'])
-                    
-                if len(ignored_rooms) != 0:
-                    form.add_error(None, f"The following room entries are skipped because they already exist in the database: {[s for s in ignored_rooms]}")
 
+                if len(ignored_rooms) != 0:
+                    form.add_error(
+                        None, f"The following room entries are skipped because they already exist in the database: {[s for s in ignored_rooms]}")
 
                 ignored_edges = set()
 
@@ -198,46 +219,59 @@ def create_rooms(request):
                     rd = row.to_dict()
 
                     if Floor.objects.filter(name=rd['start_floor']).first() is None:
-                        form.add_error('edges_csv', f"{rd['start_floor']} doesn't exist as a floor")
+                        form.add_error(
+                            'edges_csv', f"{rd['start_floor']} doesn't exist as a floor")
                         raise Exception()
-                    
+
                     if Floor.objects.filter(name=rd['end_floor']).first() is None:
-                        form.add_error('edges_csv', f"{rd['end_floor']} doesn't exist as a floor")
+                        form.add_error(
+                            'edges_csv', f"{rd['end_floor']} doesn't exist as a floor")
                         raise Exception()
-                    
-                    start_floor = Floor.objects.filter(name=rd['start_floor']).first()
-                    end_floor = Floor.objects.filter(name=rd['end_floor']).first()
+
+                    start_floor = Floor.objects.filter(
+                        name=rd['start_floor']).first()
+                    end_floor = Floor.objects.filter(
+                        name=rd['end_floor']).first()
 
                     if RoomNode.objects.filter(floor=start_floor, name=rd['start_room_name']).first() is None:
-                        form.add_error('edges_csv', f"There is no room on {rd['start_floor']} that is called {rd['start_room_name']}")
+                        form.add_error(
+                            'edges_csv', f"There is no room on {rd['start_floor']} that is called {rd['start_room_name']}")
                         raise Exception()
 
                     if RoomNode.objects.filter(floor=end_floor, name=rd['end_room_name']).first() is None:
-                        form.add_error('edges_csv', f"There is no room on {rd['end_floor']} that is called {rd['end_room_name']}")
+                        form.add_error(
+                            'edges_csv', f"There is no room on {rd['end_floor']} that is called {rd['end_room_name']}")
                         raise Exception()
 
-                    start_room = RoomNode.objects.filter(floor=start_floor, name=rd['start_room_name']).first()
-                    end_room = RoomNode.objects.filter(floor=end_floor, name=rd['end_room_name']).first()
+                    start_room = RoomNode.objects.filter(
+                        floor=start_floor, name=rd['start_room_name']).first()
+                    end_room = RoomNode.objects.filter(
+                        floor=end_floor, name=rd['end_room_name']).first()
 
-                    room_edges = RoomEdge.objects.filter(nodes=start_room).filter(nodes=end_room)
+                    room_edges = RoomEdge.objects.filter(
+                        nodes=start_room).filter(nodes=end_room)
 
                     if not room_edges.exists():
                         room_edge = RoomEdge.objects.create()
                         room_edge.nodes.add(start_room, end_room)
                     else:
-                        ignored_edges.add(f"{rd['start_floor']} {rd['start_room_name']} → {rd['end_floor']} {rd['end_room_name']}")
+                        ignored_edges.add(
+                            f"{rd['start_floor']} {rd['start_room_name']} → {rd['end_floor']} {rd['end_room_name']}")
 
                 if len(ignored_rooms) != 0:
-                    form.add_error(None, f"The following edge entries are skipped because they already exist in the database: {[s for s in ignored_edges]}")
+                    form.add_error(
+                        None, f"The following edge entries are skipped because they already exist in the database: {[s for s in ignored_edges]}")
 
                 # Otherwise a success!
-                context = {'form': form, 'success_message': 'Successfully added!'}
+                context = {'form': form,
+                           'success_message': 'Successfully added!'}
                 return render(request, 'create_floorlayout.html', context)
 
             except Exception as e:
                 # Handle other exceptions
                 print(e)
-                form.add_error(None, "An error has occurred. Please check your files over.")
+                form.add_error(
+                    None, "An error has occurred. Please check your files over.")
         else:
             # Form validation errors
             pass
@@ -245,3 +279,48 @@ def create_rooms(request):
         form = UploadRoomCSVForm()
 
     return render(request, 'create_rooms.html', {'form': form})
+
+
+def affine_transform(affine_matrix, pos):
+    return np.dot(affine_matrix, pos)[:2]
+
+
+class RoomNodeView(generics.ListAPIView):
+    queryset = RoomNode.objects.all()
+    serializer_class = RoomNodeSerializer
+
+
+class RoomEdgeView(generics.ListAPIView):
+    queryset = RoomEdge.objects.all()
+    serializer_class = RoomEdgeSerializer
+
+
+class BuildingView(generics.ListAPIView):
+    queryset = Building.objects.all()
+    serializer_class = BuildingSerializer
+
+
+class FloorView(generics.ListAPIView):
+    queryset = Floor.objects.all()
+    serializer_class = FloorSerializer
+
+
+class FloorLayoutView(generics.ListAPIView):
+    queryset = FloorLayout.objects.all()
+    serializer_class = FloorLayoutSerializer
+
+
+def history_latest_change(request):
+    watched_models = [RoomNode, RoomEdge, Building, Floor, FloorLayout]
+    results = []
+
+    for m in watched_models:
+        try:
+            results.append(m.history.latest('history_date').history_date)
+        except Exception:
+            pass # if there's no entry, we don't bother
+    
+    return JsonResponse({
+        'date': str(max(results)),
+    })
+    
