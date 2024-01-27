16b690793a920342cd4cd5bafc0fa6bde40e3724
lj2liu@uwaterloo <lj2liu@uwaterloo>
2024-01-27 00:16:47 -0500

    functionally correct: 1. admin post text/ photo, 2.accessed later from database

--- backend/adminPost/views.py
+++ backend/adminPost/views.py
@@ -12,8 +12,11 @@ from django.urls import reverse_lazy
 
 
 
-class AdminPostHomePageView(TemplateView): #home page, view list of post
+class AdminPostHomePageView(ListView):
+    model = BlogPost
     template_name = 'adminPost/home_page.html'
+    context_object_name = 'posts'  # for Html template: This is the variable used in the template for the list of posts. 
+
 
 class BlogPostListView(ListView): #retrieves a specific post
     model = BlogPost
@@ -26,23 +29,28 @@ class BlogPostDetailView(DetailView):
     context_object_name = 'post'
 
 class BlogPostCreateView(CreateView):
-
-    template_name = 'adminPost/blogpost_form.html'  # Replace with your actual template name
+    template_name = 'adminPost/blogpost_form.html'
     model = BlogPost
     form_class = BlogPostForm
-    success_url = reverse_lazy('adminPost_home')  # Replace with your actual success URL, 
-    # need to path name (3rd arg in) url pattern
+    success_url = reverse_lazy('adminPost_home')
+
+    def form_valid(self, form):
+        # Handle file upload manually
+        photo = self.request.FILES.get('photo')
+        if photo:
+            form.instance.photo = photo
+
+        # Process the form data
+        # Save the form or perform other actions
+        return super().form_valid(form)
 
     def get(self, request, *args, **kwargs):
         form = BlogPostForm()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        form = BlogPostForm(request.POST)
+        form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
-            # Process the form data
-            # Save the form or perform other actions
-
-             return super().form_valid(form) # return home page, or caller page
+            return super().form_valid(form)
         else:
             return self.form_invalid(form)
