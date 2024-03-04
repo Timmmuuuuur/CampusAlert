def3c69956f56b74c85425a9683c5289dbefe92e
lj2liu@uwaterloo <lj2liu@uwaterloo>
2024-03-04 16:32:59 -0500

    commented simple jwt

--- backend/campusalert/settings.py
+++ backend/campusalert/settings.py
@@ -60,8 +60,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework_simplejwt.authentication.JWTAuthentication', 
         # Other authentication classes if any
     ],
     
