8472bba7e5838ca5678ced5579c68d8e77c5bc81
lj2liu@uwaterloo <lj2liu@uwaterloo>
2024-03-12 17:00:49 -0400

    added 2 s paluse

--- mobile/campusalert/lib/alert/alert.dart
+++ mobile/campusalert/lib/alert/alert.dart
@@ -1,8 +1,11 @@
+import 'dart:async'; // Import the dart:async library for using Timer
+import 'dart:convert';
 import 'package:campusalert/alert/threat.dart';
 import 'package:campusalert/api_service.dart';
 import 'package:campusalert/schemas/building.dart';
 import 'package:campusalert/schemas/floor.dart';
 import 'package:campusalert/schemas/roomnode.dart';
+import 'package:http/http.dart' as http;
 
 class SyncAlert {
   final int id;
@@ -11,12 +14,13 @@ class SyncAlert {
   Floor? floor;
   RoomNode? roomNode;
 
-  SyncAlert(
-      {required this.id,
-      this.threat,
-      this.building,
-      this.floor,
-      this.roomNode});
+  SyncAlert({
+    required this.id,
+    this.threat,
+    this.building,
+    this.floor,
+    this.roomNode,
+  });
 
   Map<String, dynamic> serialize() {
     Map<String, dynamic> m = {
@@ -47,14 +51,50 @@ class SyncAlert {
     );
   }
 
-  // If there's no active alert, null is returned
+  static Timer? _timer; // Timer object to schedule periodic execution
+
+  // Method to start periodic execution of getActive()
+  static void startFetchingActive() {
+    // Cancel the previous timer if it's active
+    _timer?.cancel();
+    // Schedule periodic execution of getActive() every couple of seconds
+    _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
+      getActive(); // Call getActive() method
+    });
+  }
+
+  // Method to stop periodic execution of getActive()
+  static void stopFetchingActive() {
+    // Cancel the timer
+    _timer?.cancel();
+  }
+
+  // Method to fetch the active alert
   static Future<SyncAlert?> getActive() async {
+    print("-----reading from active----");
+
     var m = await APIService.getCommon("emergency/alert/get-active/");
+    if (m != null && m is Map<String, dynamic>) {
+      // Parse the JSON response into a Map<String, dynamic>
+    Map<String, dynamic> response = m;
+
+    // Access the value of "active_alert" from the map
+    bool activeAlert = response['active_alert'];
+
+    // Now you have the value of "active_alert"
+    print(activeAlert);
+
+    }
+    
+  
+    startFetchingActive();
     return m['active_alert'] ? await SyncAlert.deserialize(m) : null;
   }
 
+  
+
   static void createActive() {
-    APIService.postCommon("emergency/alert/create/", {});
+   APIService.postCommon("emergency/alert/create/", {});
   }
 
   static void updateActive(SyncAlert alert) {
